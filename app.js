4abd1812e30e4c4f852211692c772692024ce6db
talidoom <talidoom@yandex.ru>
2023-12-18 23:41:24 +0300

    добавление констант

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const { PORT: DEFAULT_PORT } = require('./utils/constants');
 
-const { PORT = 3000 } = process.env;
+const { PORT = DEFAULT_PORT, MONGO_URL = 'MONGO_URL', NODE_ENV } = process.env;
+const mongodb = 'mongodb://127.0.0.1:27017/bitfilmsdb';
 
 const app = express();
+app.use(cors());
+
+mongoose
+  .connect(NODE_ENV === 'production' ? MONGO_URL : mongodb)
+  .then(() => console.log('БД подключена>'))
+  .catch((err) => console.log(`Ошибка ${err} подключения к БД`));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
 
 app.listen(PORT, () => {
   console.log(`Сервер слушает ${PORT}`);
---
+++ utils/constants.js
@@ -0,0 +1,18 @@
+const STATUS_CODES = {
+  OK: 200,
+  CREATED: 201,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
+  NOT_FOUND: 404,
+  CONFLICT: 409,
+};
+const { PORT = 3000 } = process.env;
+
+const urlRegexPattern = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
+
+module.exports = {
+  PORT,
+  STATUS_CODES,
+  urlRegexPattern,
+};
