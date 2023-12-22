df46c981d2d522febbadf39e643179def0b2971d
talidoom <talidoom@yandex.ru>
2023-12-22 19:40:38 +0300

    test

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const User = require('../models/user');
+const User = require('../models/user-models');
 const { BadRequestError } = require('../errors/BadRequestError');
 const { ConflictError } = require('../errors/ConflictError');
 const { NotFoundError } = require('../errors/NotFoundError');
